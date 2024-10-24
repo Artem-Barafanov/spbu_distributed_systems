d9c545110923a62cb6941db821098117bda56382
maksson <son.mp146@gmail.com>
2024-10-24 14:33:33 +0300

    it works

--- robots.py
+++ robots.py
@@ -1,4 +1,5 @@
 import random
+from graphics import Graphics
 
 
 class Board:
@@ -10,13 +11,17 @@ class Board:
             board.append([0]*width)
         self.field = board
 
+    def __len__(self):
+        return len(self.field)
+
 
 class Robot:
-    def __init__(self, speed: int, size: int, coordinates: list[int], board: Board):
+    def __init__(self, speed: int, size: int, coordinates: list[int], board: Board, graphic: Graphics):
         self.speed: int = speed
         self.size: int = size
         self.coordinates: list[int] = coordinates
         self.board = board
+        self.graphic = graphic
 
     def walk(self, direction: str, distance: int):
         match direction:
@@ -31,9 +36,11 @@ class Robot:
 
     def paint(self):
         self.board.field[self.coordinates[1]][self.coordinates[0]] = 1
+        self.graphic.update_cells(self.board.field)
+
 
     def free_walk(self):
-        for i in range(10):
+        for i in range(100):
             direction = random.choice(["left", "right", "up", "down"])
             self.walk(direction, 1)
             self.paint()
